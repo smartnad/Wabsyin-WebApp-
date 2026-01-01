ec02a19ebca409ee389af1e2193c3d08ca37be8a
smartnad <nadsmart3@gmail.com>
2026-01-01 21:07:26 +0530

    Fix type error in DockItem using any cast

--- components/ui/dock.tsx
+++ components/ui/dock.tsx
@@ -160,7 +160,7 @@ function DockItem({ children, className }: DockItemProps) {
             aria-haspopup="true"
         >
             {Children.map(children, (child) =>
-                cloneElement(child as React.ReactElement, { width, isHovered })
+                cloneElement(child as React.ReactElement<any>, { width, isHovered })
             )}
         </motion.div>
     );
